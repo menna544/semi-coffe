86e354525b6c51108509f575a156cc6c6275670e
menna544 <116039213+menna544@users.noreply.github.com>
2023-11-09 20:10:29 +0200

    Update signinup.js

--- js/signinup.js
+++ js/signinup.js
@@ -1,7 +1,6 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
-
 var emailfield = document.getElementById("email-field");
 var emailerror = document.getElementById("email-error");
 var passerror = document.getElementById("password-error");
@@ -40,13 +39,6 @@ function validateAll() {
     
 }
 
-function myFunction() {
-    Swal.fire({
-        title: "Good job!",
-        text: "Account creater successfully!",
-        icon: "success"
-      });;
-}
 
 function contact() {
     location.replace("signinup.html");
@@ -63,3 +55,54 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-up-mode");
 });
+
+function validateForm() {
+    // Clear any previous error messages
+    const errorElements = document.querySelectorAll(".error");
+    errorElements.forEach((element) => (element.textContent = ""));
+
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const checkbox = document.getElementById("checkbox").checked;
+
+    let isValid = true;
+
+    if (username === "") {
+        document.getElementById("username-error").textContent = "Name is required";
+        isValid = false;
+    }
+    if (email === "") {
+        document.getElementById("email-error").textContent = "Email is required";
+        isValid = false;
+    }
+    if (phone === "") {
+        document.getElementById("phone-error").textContent = "Phone is required";
+        isValid = false;
+    }
+    if (password === "") {
+        document.getElementById("password-error").textContent = "Password is required";
+        isValid = false;
+    }
+    if (!checkbox) {
+        document.getElementById("checkbox-error").textContent = "You must agree to the terms";
+        isValid = false;
+    }
+
+    if (isValid) {
+        Swal.fire({
+            title: "Good job!",
+            text: "Account created successfully!",
+            icon: "success"
+          });
+          location.replace("index.html")
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "you must enter all inputs!",
+            
+          });
+    }
+}
